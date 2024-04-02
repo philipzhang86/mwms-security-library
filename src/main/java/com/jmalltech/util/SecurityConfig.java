4808853bcfd6bc7c50464c8867657a17935e6f1c
Philip Zhang <70548778+philipzhang86@users.noreply.github.com>
2024-04-02 19:26:52 -0400

    Stable version 1.11  back to stable version

---
+++ src/main/java/com/jmalltech/util/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.jmalltech.util;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public FilterRegistrationBean<JwtTokenFilter> jwtFilterRegistration() {
+        FilterRegistrationBean<JwtTokenFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new JwtTokenFilter());
+        registrationBean.addUrlPatterns("/api/*");
+        return registrationBean;
+    }
+}
