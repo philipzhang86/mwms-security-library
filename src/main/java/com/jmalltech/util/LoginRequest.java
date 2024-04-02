0d260b56382bfcda2efb7b28a9cae0b182dffdb4
Philip Zhang <70548778+philipzhang86@users.noreply.github.com>
2024-04-02 16:58:16 -0400

    Stable version 1.1

--- src/main/java/com/jmalltech/util/LoginRequest.java
+++ src/main/java/com/jmalltech/util/LoginRequest.java
@@ -1,18 +1,11 @@
 package com.jmalltech.util;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class LoginRequest {
     private String username;
     private String password;
-
-    public LoginRequest() {
-        this.username = "admin";
-        this.password = "admin";
-    }
 }
